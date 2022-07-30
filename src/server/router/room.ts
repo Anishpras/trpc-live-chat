eb7e1117c2f4aeda119160924f0b4b5f7e1521b6
anishpras <anishprashun118@gmail.com>
2022-07-30 16:54:08 +0530

    room router

--- src/server/router/room.ts
+++ src/server/router/room.ts
@@ -1,6 +1,46 @@
+import {
+  Message,
+  messageSubSchema,
+  sendMessageSchema,
+} from "./../../constants/schemas";
 import { createRouter } from "./context";
-import { z } from "zod";
+import { Events } from "../../constants/events";
+import * as trpc from "@trpc/server";
+import { nanoid } from "nanoid";
 
 export const roomRouter = createRouter()
-  .mutation("send-message", {})
-  .subscription("onSendMessage", {});
+  .mutation("send-message", {
+    input: sendMessageSchema,
+    resolve({ ctx, input }) {
+      const message: Message = {
+        id: nanoid(),
+        ...input,
+        sentAt: new Date(),
+        sender: {
+          name: ctx.session?.user?.name || "unknown",
+        },
+      };
+
+      ctx.ee.emit(Events.SEND_MESSAGE, message);
+
+      return message;
+    },
+  })
+  .subscription("onSendMessage", {
+    input: messageSubSchema,
+    resolve({ ctx, input }) {
+      return new trpc.Subscription<Message>((emit) => {
+        function onMessage(data: Message) {
+          if (input.roomId === data.roomId) {
+            emit.data(data);
+          }
+        }
+
+        ctx.ee.on(Events.SEND_MESSAGE, onMessage);
+
+        return () => {
+          ctx.ee.off(Events.SEND_MESSAGE, onMessage);
+        };
+      });
+    },
+  });
